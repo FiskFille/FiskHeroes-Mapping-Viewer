5d861bd9974a0e26aab9308ccfd4c84c80d183d9
FiskFille <fisk.randenback@gmail.com>
2022-08-26 21:32:36 +0200

    Added support for loading local mapping files

--- src/com/fiskmods/fhmv/MappingViewer.java
+++ src/com/fiskmods/fhmv/MappingViewer.java
@@ -5,7 +5,14 @@ import static com.fiskmods.fhmv.MappingGui.*;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.FileDialog;
 import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -61,6 +68,38 @@ public class MappingViewer
         selector.getContentPane().setLayout(new BorderLayout());
 
         JComboBox<String> box = font(new JComboBox<>(versions.toArray(new String[0])));
+        JButton selectOther = font(new JButton("Other"));
+        selectOther.addActionListener(e ->
+        {
+            FileDialog dialog = new FileDialog((Frame) null, "Select File", FileDialog.LOAD);
+            dialog.setDirectory(new File("").getAbsolutePath());
+            dialog.setFile("*.json");
+            dialog.setLocationRelativeTo(null);
+            dialog.setVisible(true);
+
+            String dir = dialog.getDirectory();
+            String name = dialog.getFile();
+
+            if (dir != null && name != null)
+            {
+                File file = new File(dir, name);
+
+                if (file.isFile() && file.exists())
+                {
+                    loadMappings(file);
+                    openViewer();
+                }
+            }
+        });
+
+        JPanel selection = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        selection.add(box, c);
+        c.weightx = 0;
+        selection.add(padLeft(selectOther), c);
+
         JLabel l = bold(new JLabel("Select Version"));
         l.setBorder(BorderFactory.createEmptyBorder(10, 15, 0, 10));
 
@@ -68,27 +107,44 @@ public class MappingViewer
         button.addActionListener(e ->
         {
             downloadMappings((String) box.getSelectedItem());
-            EventQueue.invokeLater(() ->
-            {
-                try
-                {
-                    selector.dispose();
-                    new MappingGui();
-                }
-                catch (Exception e1)
-                {
-                    e1.printStackTrace();
-                }
-            });
+            openViewer();
         });
 
         JPanel buttons = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 10));
         buttons.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
         buttons.add(button);
-        selector.getContentPane().add(pair(l, wrap(box)), BorderLayout.NORTH);
+        selector.getContentPane().add(pair(l, wrap(selection)), BorderLayout.NORTH);
         selector.getContentPane().add(buttons, BorderLayout.SOUTH);
         selector.setResizable(false);
         selector.setVisible(true);
+    }
+
+    public static void openViewer()
+    {
+        EventQueue.invokeLater(() ->
+        {
+            try
+            {
+                selector.dispose();
+                new MappingGui();
+            }
+            catch (Exception e1)
+            {
+                e1.printStackTrace();
+            }
+        });
+    }
+
+    public static void loadMappings(File file)
+    {
+        try
+        {
+            input = new Gson().fromJson(new FileReader(file), MappingInput.class);
+        }
+        catch (JsonSyntaxException | JsonIOException | FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void downloadMappings(String version)
@@ -107,6 +163,14 @@ public class MappingViewer
     {
         JPanel p = new JPanel(new BorderLayout());
         p.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        p.add(panel);
+        return p;
+    }
+
+    private static JPanel padLeft(Component panel)
+    {
+        JPanel p = new JPanel(new BorderLayout());
+        p.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
         p.add(panel);
         return p;
     }
